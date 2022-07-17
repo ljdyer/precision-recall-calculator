a92e0fcc87f257f631ca60bfd5288adbd9e365e4
Laurence Dyer <ljdyer@gmail.com>
2022-07-17 06:47:49 +0100

    refactor

--- src/prc_main.py
+++ src/prc_main.py
@@ -8,27 +8,10 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 
-from prc_helper import (Int_or_Str, Str_or_List, Str_or_List_or_Series,
-                        display_or_print, get_tqdm)
-
-NON_EQUAL_LENGTH_ERROR = \
-    "Hypothesis and reference lists must have equal length."
-WARNING_DIFFERENT_CHARS = """Different characters found between reference and \
-hypothesis strings in document index: {doc_idx}! \
-(Reference: "{ref_str}"; Hypothesis: "{hyp_str}"). \
-Skipping this document (returning None)."""
-INIT_COMPLETE_MSG = "Initialisation complete."
-REF_OR_HYP_TYPE_ERROR = """
-reference and hypothesis parameters must have type list, str, \
-or pandas.Series"""
-
-FEATURE_DISPLAY_NAMES = {
-    'CAPITALISATION': "Capitalisation",
-    ' ': "Spaces (' ')",
-    ',': "Commas (',')",
-    '.': "Periods ('.')",
-    'all': 'All features'
-}
+from prc_helper import (FEATURE_DISPLAY_NAMES, INIT_COMPLETE_MSG,
+                        NON_EQUAL_LENGTH_ERROR, REF_OR_HYP_TYPE_ERROR,
+                        WARNING_DIFFERENT_CHARS, Int_or_Str, Str_or_List,
+                        Str_or_List_or_Series, display_or_print, get_tqdm)
 
 tqdm_ = get_tqdm()
 
@@ -296,56 +279,6 @@ class PrecisionRecallCalculator:
         }
 
     # ====================
-    def label_fps_and_fns(self, strings: str):
-
-        output_chars = []
-        while strings['ref'] and strings['hyp']:
-            features_present = {'ref': [], 'hyp': []}
-            next_char = {
-                'ref': strings['ref'].pop(0),
-                'hyp': strings['hyp'].pop(0)
-            }
-            try:
-                assert next_char['ref'].lower() == next_char['hyp'].lower()
-            except AssertionError:
-                error_msg = WARNING_DIFFERENT_CHARS.format(
-                    doc_idx="UNKNOWN",
-                    ref_str=(next_char['ref'] + ''.join(strings['ref'][:10])),
-                    hyp_str=(next_char['hyp'] + ''.join(strings['hyp'][:10]))
-                )
-                print(error_msg)
-                return None
-            for string in strings.keys():
-                if ('CAPITALISATION' in self.features
-                   and next_char[string].isupper()):
-                    features_present[string].append('CAPITALISATION')
-                while (len(strings[string]) > 0
-                       and strings[string][0] in self.features):
-                    features_present[string].append(strings[string].pop(0))
-            if 'CAPITALISATION' in self.features:
-                if ('CAPITALISATION' in features_present['ref']
-                   and 'CAPITALISATION' not in features_present['hyp']):
-                    output_chars.append(f"\\fn{{{next_char['hyp']}}}")
-                elif ('CAPITALISATION' not in features_present['ref']
-                        and 'CAPITALISATION' in features_present['hyp']):
-                    output_chars.append(f"\\fp{{{next_char['hyp']}}}")
-                else:
-                    output_chars.append(next_char['hyp'])
-            else:
-                output_chars.append(next_char['hyp'])
-            for feature in self.feature_chars:
-                if (feature in features_present['ref']
-                   and feature not in features_present['hyp']):
-                    output_chars.append(f'\\fn{{\\mbox{{{feature}}}}}')
-                elif (feature not in features_present['ref']
-                        and feature in features_present['hyp']):
-                    output_chars.append(f'\\fp{{\\mbox{{{feature}}}}}')
-                elif (feature in features_present['ref']
-                        and feature in features_present['hyp']):
-                    output_chars.append(feature)
-        return output_chars
-
-    # ====================
     def latex_text_display(self, doc_idx: int, start_char: int = 0,
                            chars_per_row: int = 30, num_rows: int = 3):
 
@@ -355,10 +288,15 @@ class PrecisionRecallCalculator:
             'ref': list(self.reference[doc_idx].strip()),
             'hyp': list(self.hypothesis[doc_idx].strip())
         }
-        labelled = self.label_fps_and_fns(strings)
-        rows = [[labelled[i] for i in range(a, b)]
-                for (a, b) in zip(range(0, chars_per_row*num_rows, chars_per_row),
-                                  range(chars_per_row, chars_per_row*(num_rows+1), chars_per_row))]
+        labelled = self.label_fps_and_fns(
+            strings, self.features, self.feature_chars)
+        rows = [
+            [labelled[i] for i in range(a, b)]
+            for (a, b) in zip(
+                range(0, chars_per_row*num_rows, chars_per_row),
+                range(chars_per_row, chars_per_row*(num_rows+1), chars_per_row)
+            )
+        ]
         for row in rows:
             row[0] = self.escape_line_end_space(row[0])
             row[-1] = self.escape_line_end_space(row[-1])
