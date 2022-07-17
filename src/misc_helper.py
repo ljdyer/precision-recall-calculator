cc79e426e08788567045d34ad1348c035c161771
Laurence Dyer <ljdyer@gmail.com>
2022-07-17 21:01:01 +0100

    Update misc_helper.py

--- src/misc_helper.py
+++ src/misc_helper.py
@@ -88,7 +88,6 @@ def display_or_print_html(html: str):
     script is running from a notebook or not."""
 
     if is_running_from_ipython():
-        print("Here.")
-        HTML("<hl>ddd")
+        display(HTML(html))
     else:
         print(html)
