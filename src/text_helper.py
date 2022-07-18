9a72a7b8335ea4b19bd1d074ac76be282b8f1b59
Laurence Dyer <ljdyer@gmail.com>
2022-07-18 05:12:47 +0100

    Update text_helper.py

--- src/text_helper.py
+++ src/text_helper.py
@@ -53,7 +53,7 @@ def html_text_display(ref: str,
     chars = {'ref': list(ref), 'hyp': list(hyp)}
     labelled = label_fps_and_fns(chars, features,
                                  feature_chars, for_latex=False)
-    html = HTML_STYLE + pre(labelled)
+    html = HTML_STYLE + pre(''.join(labelled))
     display_or_print_html(html)
 
 
